29ed871d51a55b7a588c85d3a29eea0f7d9a7dce
FN <firenamefn@gmail.com>
2026-04-30 10:28:54 +0700

    Update Icon

--- Icon.cs
+++ Icon.cs
@@ -1,29 +1,55 @@
 using System;
+using System.Runtime.InteropServices;
 using GreenPng;
 using SDL3;
 
 namespace PanelWork;
 
-public sealed class Icon(nint handle, byte[] buffer) : IDisposable {
-    public nint Handle { get; } = handle;
+public sealed unsafe class Icon : IDisposable {
+    public nint Handle { get; }
 
-    readonly byte[] buffer = buffer;
+    readonly void* memory;
 
-    public static unsafe Icon CreateFromPixels(int width, int height, byte[] buffer) {
-        fixed(byte* pixels = buffer) {
-            nint surface = SDL.CreateSurfaceFrom(width, height, SDL.PixelFormat.ARGB8888, (nint)pixels, width * 4);
-
-            return new(surface, buffer);
-        }
+    private Icon(nint handle, void* memory) {
+        Handle = handle;
+        this.memory = memory;
     }
 
-    public static Icon CreateFromPng(ReadOnlySpan<byte> png) {
-        byte[] buffer = PngDecoder.Decode(png, out PngHeader header);
+    public static Icon CreateFromPixels(int width, int height, ReadOnlySpan<byte> span) {
+        void* memory = NativeMemory.Alloc((nuint)span.Length);
 
-        return CreateFromPixels(header.Width, header.Height, buffer);
+        fixed(byte* spanPointer = span)
+            NativeMemory.Copy(spanPointer, memory, (nuint)span.Length);
+
+        nint surface = SDL.CreateSurfaceFrom(width, height, SDL.PixelFormat.ARGB8888, (nint)memory, width * 4);
+
+        return new(surface, memory);
+    }
+
+    public static bool TryCreateFromPng(ReadOnlySpan<byte> png, out Icon icon) {
+        icon = null;
+
+        if(!PngDecoder.TryDecodeHeader(png, out PngHeader header))
+            return false;
+
+        if(!PngDecoder.IsHeaderSupported(header))
+            return false;
+
+        void* memory = NativeMemory.Alloc((nuint)header.ByteSize);
+
+        if(!PngDecoder.TryDecode(png, header, new(memory, header.ByteSize)))
+            return false;
+
+        nint surface = SDL.CreateSurfaceFrom(header.Width, header.Height, SDL.PixelFormat.ARGB8888, (nint)memory, header.Width * 4);
+
+        icon = new(surface, memory);
+
+        return true;
     }
 
     public void Dispose() {
         SDL.DestroySurface(Handle);
+
+        NativeMemory.Free(memory);
     }
 }
